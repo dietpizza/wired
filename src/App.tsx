56ffc496318d68d0ff4f29b35b7ee5b3e0a0a005
Rohan Biswas <brainhacker550@gmail.com>
2021-09-08 02:17:39 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import { io, Socket } from 'socket.io-client';
 
-const socket: Socket = io('https://localhost:4430');
-
 function App() {
-    useEffect(() => {
-        socket.on('connect', () => console.log('Connected'));
+    const [connect, setConnect] = useState(false);
+
+    function onMount() {
+        const socket: Socket = io('https://localhost:1337', {
+            reconnectionDelayMax: 1000,
+        });
+
+        socket.on('connect', () => setConnect(true));
+        socket.on('disconnect', () => setConnect(false));
         socket.on('data', console.log);
-    });
+        return () => {
+            socket.disconnect();
+        };
+    }
+
+    useEffect(onMount, []);
 
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <img src={connect ? logo : ''} className="App-logo" alt="logo" />
+                <p>{connect ? 'Connected!' : 'Disconnected!'}</p>
             </header>
         </div>
     );
