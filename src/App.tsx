d4a0e3a940ac275b4cce1c6bb4ab87093127ad09
Rohan Biswas <brainhacker550@gmail.com>
2021-09-07 22:44:57 +0530

    naisu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { io, Socket } from 'socket.io-client';
+
+const socket: Socket = io('https://localhost:4430');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        socket.on('connect', () => console.log('Connected'));
+        socket.on('data', console.log);
+    });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
